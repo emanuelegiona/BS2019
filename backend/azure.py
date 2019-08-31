0d1a8448940fa21a59f31aff7460c371b75f1eb2
Emanuele Giona <giona.emanuele@gmail.com>
2019-08-31 10:02:40 +0200

    Azure clients development started

--- backend/azure.py
+++ backend/azure.py
@@ -1,3 +1,25 @@
 """
 This file contains source code for every Microsoft Azure interaction involved in this project.
 """
+from rest_client import RESTClient
+
+
+class CredentialsManager:
+    """
+    This class implements an Azure credentials manager, handling their loading from a CSV file and exposing methods to access them.
+    """
+    pass
+
+
+class SpeechToTextClient:
+    """
+    This class implements an Azure client for the Speech REST API.
+    """
+    pass
+
+
+class IdentificationClient:
+    """
+    This class implements an Azure client for the Speaker Recognition REST API regarding the identification task.
+    """
+    pass
