ab63e5f90e0b5ba5a0fe1b540bb3662dca3b9a9e
Emanuele Giona <giona.emanuele@gmail.com>
2019-08-31 12:36:02 +0200

    POST method updated to accept JSON, binary, or no body at all

--- backend/rest_client.py
+++ backend/rest_client.py
@@ -5,6 +5,7 @@ from collections import namedtuple
 import requests
 from requests.exceptions import RequestException
 from json.decoder import JSONDecodeError
+import typing
 import json
 
 """
@@ -71,11 +72,12 @@ class RESTClient:
 
         return SimpleResponse(code=code, content=content, message=msg)
 
-    def post(self, url: str, body: json, headers: dict = None, params: dict = None, expect_json: bool = True) -> SimpleResponse:
+    def post(self, url: str, body: json = None, data: typing.BinaryIO = None, headers: dict = None, params: dict = None, expect_json: bool = True) -> SimpleResponse:
         """
         Performs an HTTP POST request.
         :param url: URL to which perform the POST request to
-        :param body: JSON content to be POSTed to the URL
+        :param body: JSON content to be POSTed to the URL; mutually exclusive with data
+        :param data: binary content to be POSTed to the URL; mutually exclusive with body
         :param headers: HTTP headers to be specified
         :param params: HTTP parameters to be passed in the URL querystring
         :param expect_json: if True, the client expects a JSON content response and will try to parse it, otherwise it will read plain HTML from the web page
@@ -85,10 +87,20 @@ class RESTClient:
         content = None
         msg = ""
         try:
-            req = requests.post(url=url,
-                                headers=headers,
-                                params=params,
-                                json=body)
+            if body is not None and data is not None:
+                raise RequestException("Parameters 'body' and 'data' are mutually exclusive.")
+
+            req = None
+            if body is not None:
+                req = requests.post(url=url,
+                                    headers=headers,
+                                    params=params,
+                                    json=body)
+            elif data is not None:
+                req = requests.post(url=url,
+                                    headers=headers,
+                                    params=params,
+                                    data=data)
 
             code = req.status_code
             msg = req.reason
@@ -108,9 +120,9 @@ class RESTClient:
         except TimeoutError:
             content = None
             msg = "Timeout"
-        except RequestException:
+        except RequestException as e:
             content = None
-            msg = "General error"
+            msg = str(e)
 
         return SimpleResponse(code=code, content=content, message=msg)
 
