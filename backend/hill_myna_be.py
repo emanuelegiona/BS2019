6255e7c404f27758e1d48c74dad5a370bbb6fe4c
Emanuele Giona <giona.emanuele@gmail.com>
2019-09-03 23:29:01 +0200

    operation status check implemented; half-finished draft for profile management methods

--- backend/hill_myna_be.py
+++ backend/hill_myna_be.py
@@ -55,8 +55,125 @@ class HillMyna:
         creds = self.__credentials_manager.get(identification_resource)
         self.__SpeakerClient = IdentificationClient(credentials=creds, debug=self.__debug)
 
+    # --- General ---
+    def operation_status(self, url: str, enrollment: bool = False, identification: bool = True) -> str or (str, str):
+        """
+        Checks the outcome of either an enrollment or an identification operation. The two are MUTUALLY EXCLUSIVE.
+        In the case of an enrollment operation: returns a string containing the status of the enrollment phase.
+        In the case of an identification operation: returns a tuple of strings in the format (Azure ID, confidence).
+        ATTENTION: an Azure ID equal to '00000000-0000-0000-0000-000000000000' means NO MATCH for the set of candidate IDs.
+        :param url: URL to query for updates, either associated to an enrollment operation, or to an identification one
+        :param enrollment: if True, the operation to check is an enrollment one (MUTUALLY EXCLUSIVE, default: False)
+        :param identification: if True, the operation to check is an identification one (MUTUALLY EXCLUSIVE, default: True)
+        :return: string or tuple of strings (according to the operation type) encoding the status of the operation
+        """
+
+        assert not (enrollment and identification), "Enrollment and identification are two mutually exclusive operations."
+        assert enrollment or identification, "An operation must be provided."
+
+        ret = ""
+        json_response = self.__SpeakerClient.operation_status(operation_id=url)     # TODO check url != operation ID
+        status = json_response["status"]
+        if status == "failed":
+            # fetch the error message
+            msg = json_response["message"]
+            ret = "The operation is failed with message: {msg}.".format(msg=msg)
+
+        elif status == "running":
+            ret = "The operation is running."
+
+        elif status == "notstarted":
+            ret = "The operation is not started."
+
+        elif status == "succeeded":
+            # fetch the actual result of the operation
+            json_response = json_response["processingResult"]
+
+            # go through all the enrolment cases
+            if enrollment:
+                status = json_response["enrollmentStatus"]
+                if status == "Enrolling":
+                    # fetch the remaining time in order to complete enrollment
+                    remaining_time = json_response["remainingEnrollmentSpeechTime"]
+                    ret = "Enrollment phase can be completed with {time} more seconds.".format(time=remaining_time)
+
+                elif status == "Training":
+                    ret = "Profile is currently in training phase and will be soon ready for identification."
+
+                elif status == "Enrolled":
+                    ret = "Successfully enrolled."
+
+            # go through all the identification cases
+            elif identification:
+                azure_id = json_response["identifiedProfileId"]     # No-match ID: "00000000-0000-0000-0000-000000000000"
+                confidence = json_response["confidence"]
+
+                ret = (azure_id, confidence)
+
+        return ret
+    # --- --- ---
+
     # --- Profile management ---
     # creation, deletion, enrolment, check status, list all profiles
+    def new_profile(self, username: str, name: str, surname: str) -> None:
+        """
+        Creates a new user profile and associates a new Azure ID to it.
+        :param username: User-chosen nickname
+        :param name: User's first name
+        :param surname: User's last name
+        :return: None
+        """
+
+        assert username is not None, "Username must be provided."
+        assert name is not None, "Name must be provided."
+        assert surname is not None, "Surname must be provided."
+
+        azure_id = self.__SpeakerClient.new_profile()
+        # TODO user management: add user profile
+
+    def delete_profile(self, username: str = None, azure_id: str = None) -> None:
+        """
+        Deletes an existing user profile, referenced either by its username or Azure ID.
+        If both are provided, the referenced profile MUST be the same.
+        :param username: User-chosen nickname (Optional)
+        :param azure_id: Azure ID (Optional)
+        :return: None
+        """
+
+        assert not (username is None and azure_id is None), "Either a username or an Azure ID must be provided."
+
+        if username is not None and azure_id is not None:
+            # TODO user management: check username and azure_id refer to the same user profile
+            pass
+
+        # TODO user management: find azure_id if only username is provided
+        if self.__SpeakerClient.del_profile(profile_id=azure_id):
+            # TODO user management: delete user profile
+            pass
+
+    def enrollment(self, audio_path: str, username: str = None, azure_id: str = None, short_audio: bool = False) -> str:
+        """
+        Associates a new enrollment procedure to an existing profile, referenced either by its username or Azure ID.
+        If both are provided, the referenced profile MUST be the same.
+        :param audio_path: Path to the audio file
+        :param username: User-chosen nickname (Optional)
+        :param azure_id: Azure ID (Optional)
+        :param short_audio: if True, audio can be as short as 1 second; otherwise, 5 seconds (Optional, default: False)
+        :return: URL to query for updates on this enrolment request status
+        """
+
+        assert not (username is None and azure_id is None), "Either a username or an Azure ID must be provided."
+
+        if username is not None and azure_id is not None:
+            # TODO user management: check username and azure_id refer to the same user profile
+            pass
+
+        # TODO user management: find azure_id if only username is provided
+        url = self.__SpeakerClient.new_enrollment(profile_id=azure_id,
+                                                  audio_path=audio_path,
+                                                  short_audio=short_audio)
+
+        return url
     # --- --- ---
 
     # --- Logging in ---
