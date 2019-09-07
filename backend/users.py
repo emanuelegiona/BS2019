1c34c7b81915b2231b92adf1a4f5c70bcafb2a2e
Emanuele Giona <giona.emanuele@gmail.com>
2019-09-07 10:46:16 +0200

    simplified several methods

--- backend/users.py
+++ backend/users.py
@@ -3,12 +3,12 @@ This file contains all the source code needed to handle and query user profiles 
 """
 
 from collections import namedtuple
-from datetime import datetime
 from typing import List
-import time
 import os.path
 import json
+
 User = namedtuple("User", "azure_id username name surname status")
+
 
 class UsersManager:
 
@@ -21,7 +21,7 @@ class UsersManager:
         :param users_path: Path to the users file
         """
 
-        # Creation of a dictionary for future use, feach entry formed by a string and a User
+        # Creation of a dictionary for future use, each entry formed by a string and a User
         # self.UsId: {} : add if in need of a second dictionary
 
         self.AzId = {}
@@ -37,13 +37,12 @@ class UsersManager:
         # read from json file and AzId fill
 
         with open(users_path, "r") as read_file:
-            users= json.load(read_file)
+            users = json.load(read_file)
             for user in users:
                 person = User(user, user["username"], user["name"], user["surname"], user["status"])
                 self.AzId[user] = person
 
-    def remove (self, identifier : str) -> None:
-
+    def remove(self, identifier: str) -> None:
         """
         Removes a user from the dictionary
         :param identifier: String containing the azure_id to remove
@@ -51,9 +50,10 @@ class UsersManager:
         """
         if identifier in self.AzId:
             self.AzId.pop(identifier)
-        else : raise KeyError("The user that should be eliminated does not exist")
+        else:
+            raise KeyError("The user that should be eliminated does not exist")
 
-    def add(self, azure_id : str, username: str, name : str, surname : str, status : bool) -> None:
+    def add(self, azure_id: str, username: str, name: str, surname: str, status: bool) -> None:
         """
         Adds a user into a dictionary:
         :param azure_id: Azure id of the new user
@@ -65,52 +65,52 @@ class UsersManager:
         """
         if azure_id in self.AzId:
             raise KeyError("Azure_id already existing, insert a new one in order to register the new user")
-        for x in self.AzId:
-            if username in self.AzId["username"]:
+        for _, values in self.AzId.values():
+            if username == values.username:
                 raise KeyError("Username already taken, please choose one that has not been chosen")
 
         person = User(azure_id, username, name, surname, status)
         self.AzId[azure_id] = person
 
-    def get_Azure_Id(self, azure_id) -> User:
+    def get_by_azure_id(self, azure_id: str) -> User:
         """
         Returns every information about a user stored in the database
-        :param azureId: contains the Id of a user in order to search for its informations
+        :param azure_id: contains the Id of a user in order to search for its information
         :return: Returns a dictionary containing every information of a certain user
         """
         if azure_id in self.AzId:
-            return User(self.AzId.get(azure_id).azure_id, self.AzId.get(azure_id).username, self.AzId.get(azure_id).name
-                        , self.AzId.get(azure_id).surname,self.AzId.get(azure_id).status)
-        else : raise KeyError("The requested user does not exist in the db. Pls retry with an existent one")
+            return self.AzId[azure_id]
+        else:
+            raise KeyError("The requested user does not exist in the db. Pls retry with an existent one")
 
-    def get_Username(self, username) -> User:
+    def get_by_username(self, username: str) -> User:
         """
         Returns every information about a user stored in the database
         :param username: contains the username of a user in order to search for its informations
         :return : Returns a dictionary containing every information of a certain user
         """
-        for id, value in self.AzId.values() :
-            if username == self.AzId.get(id).username:
-                return self.get_Azure_Id(self.AzId.get(id).azure_id)
+        for _, current_user in self.AzId.values():
+            if username == current_user.username:
+                return current_user
         raise KeyError("No user with such username exists. please retry with an existing one")
 
-    def ten_Users_List (self) -> List:
+    def get_all_users(self) -> List[List[User]]:
         """
-        The function returns a list of lists, containing each one at most 1o Users.
+        The function returns a list of lists, containing each one at most 10 Users.
         :return: A list of lists, each one containing at most 10 Users
         """
-        x, y = 0
-        bunch = []
-        aux = []
-        # for cycles AzId and takes every user, dividing them in groups of 10 users at most
-        for user in self.AzId:
-            if y >= 10:
-                bunch.append(aux.copy())
-                y = 0
-            person = self.AzId.get(user)
-            aux.append(person.copy())
-            x = x + 1
-        return list
+        all_users = []
+        batch = []
+        for _, current_user in self.AzId.values():
+            batch.append(current_user)
+
+            if len(batch) == 10:
+                all_users.append(batch)
+                batch = []
+        if len(batch) > 0:
+            all_users.append(batch)
+
+        return all_users
 
 
 
